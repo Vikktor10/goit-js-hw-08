771c1df0680a0d8cf039cfb6c5a274fd8ed0ba35
Vikktor10 <hirzhev_victor@ukr.net>
2022-08-04 10:24:54 +0300

    commit 1, 2 hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const gallery = document.querySelector('.gallery');
+
+const imageEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src='${preview}'
+          alt='${description}'
+        />
+      </a>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', imageEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+console.log(gallery);
 console.log(galleryItems);
